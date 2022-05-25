ce5604a65990ec295fbe9c1441017e8c5acb23a8
Reduwan-Nyx <arnodorain124@gmail.com>
2022-05-25 20:26:37 +0600

    user added

--- index.js
+++ index.js
@@ -37,6 +37,18 @@ async function run(){
         const servicesCollection = client.db('cart_tool').collection('services')  
         const bookingCollection = client.db('cart_tool').collection('booking')
         const userCollection = client.db('cart_tool').collection('users')
+        const purchaseCollection = client.db('cart_tool').collection('purchase')
+
+      const verifyAdmin = async(req,res, next) =>{
+          const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({email: requester})
+          if(requesterAccount.role === 'admin'){
+            next()
+          } else{
+            res.status(403).send({message: 'forbidden'})
+          }
+      }
+
 
 
       // product
@@ -73,11 +85,8 @@ async function run(){
         })
 
         // admin
-        app.put('/user/admin/:email', verifyJWT, async(req, res)=>{
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async(req, res)=>{
           const email = req.params.email;
-          const requester = req.decoded.email;
-          const requesterAccount = await userCollection.findOne({email: requester})
-          if(requesterAccount.role === 'admin'){
             const filter = {email : email};
             const updateDoc = {
               $set: {role: 'admin'},
@@ -85,10 +94,7 @@ async function run(){
             const result = await userCollection.updateOne(filter, updateDoc);
            
             res.send(result)
-          }
-          else{
-            res.status(403).send({message: 'forbidden'})
-          }
+        
         })
   
 
@@ -108,6 +114,13 @@ async function run(){
       })
 
 
+
+      // purchase
+      app.post('/purchase', verifyJWT, verifyAdmin, async(req, res)=>{
+        const purchase = req.body;
+        const reuslt = await purchaseCollection.insertOne(purchase)
+        res.send(reuslt)
+      })
   
 
     }
